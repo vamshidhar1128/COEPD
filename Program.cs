02d76755b4ce372bfacafe432d90c4b16a51509f
vamshidhar1128 <164383145+vamshidhar1128@users.noreply.github.com>
2025-08-04 15:16:29 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
-// COEPD-DOTNET/Program.cs
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from COEPD!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
 app.Run();
