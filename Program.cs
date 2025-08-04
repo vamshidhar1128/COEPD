2d816937a9d183191428f225a280e3ddb0271888
vamshidhar1128 <164383145+vamshidhar1128@users.noreply.github.com>
2025-08-04 15:27:05 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
-app.MapRazorPages();
-
+...
 app.Run();
